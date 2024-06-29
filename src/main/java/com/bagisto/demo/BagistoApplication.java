5bab4e28dc56eacc8c7530c7174360524e2dc37f
AKASH KASHYAP <surajkashyap9911@gmail.com>
2024-06-29 22:00:39 +0530

    refactor html codebase.

--- src/main/java/com/bagisto/demo/BagistoApplication.java
+++ src/main/java/com/bagisto/demo/BagistoApplication.java
@@ -7,5 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BagistoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BagistoApplication.class, args);
+
+		System.out.print("http://localhost:3000/users");
 	}
 }
